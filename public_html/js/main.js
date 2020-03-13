604d054033c1a28684816474d7054c8485e27be9
Marko Petronijevic <markobank87@gmail.com git config --global user.name Bankela>
2020-03-13 22:22:08 +0100

    section about end

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
     
+    function checkHeader(){
+        let headerHeight=$('header').innerHeight();
+        $('main').css('margin-top', headerHeight);
+       
+    }
+     checkHeader();
+     $(window).resize(function(){
+         checkHeader();
+     });
     
-    
-    
-})
+});
 
