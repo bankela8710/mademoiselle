5e6591659bbcc1b127c8d4a6ef045421b33361e0
Marko Petronijevic <markobank87@gmail.com git config --global user.name Bankela>
2020-03-16 19:17:04 +0100

    index.html done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -12,3 +12,31 @@ $(document).ready(function(){
     
 });
 
+function animation() {
+        var windowHeight = $(window).height();
+        var scroll = $(window).scrollTop();
+        $('.animation').each(function () {
+            let position = $(this).offset().top;
+            let animation = $(this).attr('data-animation');
+            let delay = $(this).attr('data-delay');
+            if (position < scroll + windowHeight - 100) {
+                $(this).css('animation-delay', delay);
+                $(this).addClass(animation);
+            }
+        });
+    }
+    animation();
+    $(window).scroll(function () {
+        animation();
+        animateHeader();
+    });
+    
+    if($('.blog-slider').length > 0){
+        $('.blog-slider').owlCarousel({
+            //autoplay: true,
+            //loop:true,
+            items:1
+           
+        });
+    }
+
